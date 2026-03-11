bc85bd872459129d5d27dd61bd866d3d1b9af5e0
HenrikBerg99 <henrikberg9@gmail.com>
2026-03-11 18:43:30 +0100

    1

---
+++ oblig_2/Episode.java
@@ -0,0 +1,27 @@
+public class Episode {
+
+    // Instansvariabler
+    private String title;
+    private int episodeNumber;
+    private int seasonNumber;
+    private int runtime;
+
+    // Konstruktør
+    public Episode(String title, int episodeNumber, int seasonNumber, int runtime) {
+        this.title = title;
+        this.episodeNumber = episodeNumber;
+        this.seasonNumber = seasonNumber;
+        this.runtime = runtime;
+    }
+
+    // Setters
+    public String getTitle() {return title;}
+    public int getEpisodeNumber() {return episodeNumber;}
+    public int getSeasonNumber() {return seasonNumber;}
+    public int getRuntime() {return runtime;}
+
+    // Getters
+    public void setTitle(String title) {this.title = title;}
+    public void setEpisdeNumber
+
+}
